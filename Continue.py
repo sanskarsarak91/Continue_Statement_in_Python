29057036abe94e3bc7ec6932d28f40dfe8067e56
Sanskar Sarak <sanskarsarak91@gmail.com>
2026-08-27 22:14:42 +0530

    Add files via upload

---
+++ Continue.py
@@ -0,0 +1,199 @@
+
+#======================== Continue Statement ===============================
+
+#1. print number from 1 to 20, but skip 5.
+# a=1
+# while a<=10:
+#     a+=1
+#     if a==5:
+#         continue
+#     print(a)
+
+#2. print number from 1 to 30 , but skip all numbers divisible by 3.
+# num=1
+# while True:
+#     if num<=30:
+        
+#         if num%3==0:
+#             num+=1
+#             continue
+#         print(num)
+#         num+=1
+
+#3. print numbers from 1 to 20, but skip even number.
+# a=1
+# while a<=20:
+#     if a%2==0:
+#         a+=1
+#         continue
+#     print(a)
+#     a+=1
+
+#4. print numbers from 1 to 20 , but skip odd numbers.
+# count=1
+# while True:
+#     if count<=20:
+#         if count%2!=0:
+#             count+=1
+#             continue
+#         print(count)
+#         count+=1
+
+#5. print numbers from 1 to 50, but skip numbers divisible by 5. 
+# a=1
+# while a<=50:
+#     if a%5==0:
+#         a+=1
+#         continue
+#     print(a)
+#     a+=1
+
+#6. print numbers from 10 to 30 , but skip 15.
+# a=10
+# while a<=30:
+   
+#     if a==15:
+#         a+=1
+#         continue
+#     print(a)
+#     a+=1
+
+#7. print numbers from 1 to 20, but skip numbers 7 and 14.
+# a=1
+# while True:
+#     if a<=20:
+#         if a==7 or a==14:
+#             a+=1
+#             continue
+#         print(a)
+#         a+=1
+        
+#8. print numbers from 1 to 50 but skip divisible by 10.
+# a=1
+# while a<=50:
+#     if a%10==0:
+#         a+=1
+#         continue
+#     print(a)
+#     a+=1
+
+#9. print numbers from 1 to 30, but print only numbers that are not divisible by 4.
+# a=1
+# while a<=30:
+#     if a%4==0:
+#         a+=1
+#         continue
+#     print(a)
+#     a+=1
+
+#10. print number from 20 down to 1, but skip 10.
+# a=20
+# while a>=1:
+#     if a==10:
+#         a-=1
+#         continue
+#     print(a)
+#     a-=1
+
+#11. print numbers from 1 to 100, but skip divisible by 7.
+# num=1
+# while num<=100:
+#     if num%7==0:
+#         num+=1
+#         continue
+#     print(num)
+#     num+=1
+
+#12. print numbers from 1 to 30 , but skip all even numbers.
+# num=1
+# while num<=30:
+#     if num%2==0:
+#         num+=1
+#         continue
+#     print(num)
+#     num+=1
+
+#13. print numbers from 1 to 50, but skip numbers divisible by both 3 and 5.
+# p=1
+# while True:
+#     if p<=50:
+#         if p%3==0 or p%5==0:
+#             p+=1
+#             continue
+#         print(p)
+#         p+=1
+
+#14. print numbers from 1 to 20, but skip numbers from 8 to 12.
+# q=1
+# while q<=20:
+#     if q==8 or q==12:
+#         q+=1
+#         continue
+#     print(q)
+#     q+=1
+
+#15. print numbers from 1 to 40, but skip multiples of 6.
+# a=1
+# while a<=40:
+#     if a%6==0:
+#         a+=1
+#         continue
+#     print(a)
+#     a+=1
+
+#16. print numbers from 1 to 50, but skip numbers divisible by 3 or 5.
+# a=1
+# while a<=50:
+#     if a%3==0 or a%5==0:
+#         a+=1
+#         continue
+#     print(a)
+#     a+=1
+
+#17. print numbers from 1 to 100, but skip all even numbers and numbers divisible by 7.
+# num=1
+# while True:
+#     if num<=100:
+#         if num%2==0 or num%7==0:
+#             num+=1
+#             continue
+#         print(num)
+#         num+=1
+
+#18. take number from the user one by one. skip negative numbers and continue asking until the user enters 0.
+
+# num=int(input("Enter the Number and outoff code enter 0:"))
+# while True:
+#     if num==0:
+#        print("Out of code")
+#        break
+        
+#     if num>0:
+#         num+=1
+#         continue
+# print(num)
+# num+=1
+
+#19. print numbers from 1 to 100, but skip numbers containing the digit.
+# num=1
+# while num<=100:
+#     if "5" in str(num):
+#         num+=1
+#         continue
+#     print(num)
+#     num+=1
+
+#20. print numbers from 1 to 100, but skip numbers ending in 0.
+# num=1
+# while num<=100:
+#     if num%10==0:
+#         num+=1
+#         continue
+#     print(num)
+#     num+=1
+
+#============================== Thank You !! =======================
+   
+
+    
+
